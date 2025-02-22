7c844448139d225bbbb67b3aa7b2f1e2a48dc46f
F-A-I-S-A-L-14 <ariffaisal895@gmail.com>
2025-02-23 00:44:49 +0600

    Assignment solving

---
+++ Class 1 assignment/Prob 2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("My Name is x\nMy mother name is y\nMy mother name is z\n");
+    return 0;
+}
---
+++ Class 1 assignment/Prob 4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char word1='A', word2='R', word3='I', word4='F', word5='S', word6='L';
+    printf("%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n%c\n", word1, word2, word3, word4, word4, word1, word3, word5, word1, word6);
+    return 0;
+}
